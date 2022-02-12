83e61ea13b905f1e47c8284607e39cac12c18608
mgOh <83509684+OMG-maker@users.noreply.github.com>
2022-02-12 22:47:14 +0900

    add: BackJoon_4726 (캠핑 - 사칙연산)

---
+++ omgMaker/4796.py
@@ -0,0 +1,20 @@
+import sys
+
+i = 0
+
+
+def is_big(d, r1):
+    if d > r1:
+        return r1
+    else:
+        return d
+
+
+while True:
+    i += 1
+    r1, r2, r3 = map(int, sys.stdin.readline().split())
+    if r1 + r2 + r3 == 0:
+        break
+    else:
+        result = (r1 * int(r3 / r2)) + is_big(r3 % r2, r1)
+        print('Case ' + str(i) + ': ' + str(result))
