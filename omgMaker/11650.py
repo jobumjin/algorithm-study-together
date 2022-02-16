626883a655cc6c602b47297e902d75fba04973b8
mgOh <issac_oh@naver.com>
2022-02-16 23:00:28 +0900

    add: 11650 (정렬)

---
+++ omgMaker/11650.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+arr = []
+
+for i in range(N):
+    arr.append(list(map(int, input().split())))
+
+arr.sort()
+
+for i in range(N):
+    print(arr[i][0], arr[i][1])
