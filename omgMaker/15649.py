ba7fd34af81239ff718b916daf717a33de3672ce
mgOh <issac_oh@naver.com>
2022-02-17 23:24:09 +0900

    add: 15649 - N과 M (백트래킹)

---
+++ omgMaker/15649.py
@@ -0,0 +1,18 @@
+import sys
+from itertools import permutations  # 순열
+
+N, M = map(int, sys.stdin.readline().split())
+
+v = []
+
+for i in range(1, N+1):
+    v.append(i)
+
+tu = permutations(v, M)
+arr = list(tu)
+
+for i in arr:
+    result = ''
+    for j in i:
+        result += str(j) + ' '
+    print(result)
